f0474a4dc3952258e3e6632875b2692b1c7108a0
Eleanor Lee <elee@elee-01.local>
2015-03-16 17:05:56 -0500

    add spec_helper and fix create_animal factory spec

--- spec/animal/animal_factory_spec.rb
+++ spec/animal/animal_factory_spec.rb
@@ -1,11 +1,11 @@
-require 'model_helper'
+require 'spec_helper'
 require 'animal/animal_factory'
 
 describe AnimalLibrary::AnimalFactory do
   subject { described_class }
 
   it 'constructs create_animal_factory context' do
-    expect(subject.create_animal).to be_a(AnimalLibrary::AnimalFactory::Context::CreateAnimal)
+    expect(subject.create_animal).to be_a(AnimalLibrary::Context::CreateAnimal)
   end
 
 end
