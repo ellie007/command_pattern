ab440a402359033255203c20f3a90ba36cb21c8e
Eleanor Lee <elee@elee-01.local>
2015-03-16 09:45:44 -0500

    fixed form for animal submission and create template issue

--- app/controllers/animals_controller.rb
+++ app/controllers/animals_controller.rb
@@ -13,7 +13,14 @@ class AnimalsController < ApplicationController
 
   def create
     @animal_context = AnimalLibrary::AnimalFactory.create_animal
-    @animal = @animal_context.execute(params)
+    @animal = @animal_context.execute(animal_params)
+    redirect_to animals_path
+  end
+
+  private
+
+  def animal_params
+    params.permit(:name)
   end
 
 end
